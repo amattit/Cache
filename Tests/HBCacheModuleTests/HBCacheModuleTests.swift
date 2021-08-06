4c9b4bc2be4960f6c68594adf16355e01b5f79b9
Mikhail Seregin <mikhail.a.seregin@gmail.com>
2021-08-06 17:04:43 +0300

    add tests

--- Tests/HBCacheModuleTests/HBCacheModuleTests.swift
+++ Tests/HBCacheModuleTests/HBCacheModuleTests.swift
@@ -2,10 +2,86 @@
     @testable import HBCacheModule
 
     final class HBCacheModuleTests: XCTestCase {
-        func testExample() {
-            // This is an example of a functional test case.
-            // Use XCTAssert and related functions to verify your tests produce the correct
-            // results.
-            XCTAssertEqual(HBCacheModule().text, "Hello, World!")
+        
+        
+        
+        func testEmptyCache() {
+            let cache = Cache<String, [Int]>()
+            guard let _ = cache.value(forKey: "empty") else {
+                XCTAssert(true)
+                return
+            }
+            XCTFail()
+        }
+        
+        func testAddValue() {
+            let cache = Cache<String, [Int]>()
+            let array = [1, 2, 3, 4, 5]
+            cache["first"] = array
+            guard let data = cache["first"] else {
+                XCTFail()
+                return
+            }
+            for i in 0 ..< array.count {
+                XCTAssert(array[i] == data[i])
+            }
+        }
+        
+        func testRemoveValue() {
+            let cache = Cache<String, [Int]>()
+            let array = [1, 2, 3, 4, 5]
+            cache["first"] = array
+            cache.removeValue(forKey: "first")
+            guard let _ = cache["first"] else {
+                XCTAssertTrue(true)
+                return
+            }
+            XCTFail()
+        }
+        
+        func testReplaceData() {
+            let cache = Cache<String, [Int]>()
+            let array = [1, 2, 3, 4, 5]
+            let array2 = array.reversed().compactMap { $0 }
+            
+            cache["first"] = array
+            cache["first"] = array2
+            
+            guard let data = cache["first"] else {
+                XCTFail()
+                return
+            }
+            
+            for i in 0 ..< array.count {
+                XCTAssert(array2[i] == data[i])
+            }
+        }
+        
+        func testSaveToFile() {
+            let cache = Cache<String, [Int]>()
+            let array = [1, 2, 3, 4, 5]
+            cache["first"] = array
+            do {
+                try cache.saveToDisk(withName: "first")
+            } catch {
+                XCTFail(error.localizedDescription)
+            }
+        }
+        
+        func testloadFromFile() {
+            let cache = Cache<String, [Int]>()
+            let array = [1, 2, 3, 4, 5]
+            cache["first"] = array
+            do {
+                try cache.saveToDisk(withName: "first")
+                
+                let c = try cache.loadFromDisk(withName: "first").value(forKey: "first")!
+                for i in 0 ..< array.count {
+                    XCTAssert(array[i] == c[i])
+                }
+                
+            } catch {
+                XCTFail(error.localizedDescription)
+            }
         }
     }
